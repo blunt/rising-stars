48241f0d7dc455a814feedf5fdb4535959843d24
Maxim Siebert <maxim.siebert@gmail.com>
2019-06-27 20:51:47 -0400

    initial

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = {
+  templateFormats: [
+    "html",
+    "md",
+    "jpg",
+    "png",
+    "svg",
+    "xml",
+    "ico",
+    "css",
+    "webmanifest",
+  ],
+  passthroughFileCopy: true,
+};
