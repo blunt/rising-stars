f9943d4c4bd9aad71efd55187475ca573b8eac15
Maxim Siebert <maxim.siebert@gmail.com>
2019-07-02 22:48:26 -0400

    homepage, global, responsive

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Lato', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+        'serif': ['Roboto Slab', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+      },
+      colors: {
+        blue: {
+          '700': '#0057B8',
+          '800': '#024DA0',
+          '900': '#00062E'
+        },
+        yellow: {
+          '700': '#FCB354',
+          '800': '#EA9B35'
+        }
+      }
+    }
+  },
+  variants: {
+  },
+  plugins: [
+    // Some useful comment
+  ]
+};
